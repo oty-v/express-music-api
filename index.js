ef2febacc1c2e28c86df287edeb2f6dc6f6d8d77
Misha <43975516+oty-v@users.noreply.github.com>
2021-02-13 00:24:02 +0200

    created

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ app.listen(port, () => {
 });
 
 /////get request handel here get url for one video
+app.get("/", (req, res) => {
+  res.end(`<h1>Home</h1>`);
+});
 
 app.get("/youtube", (req, res) => {
   const URL = req.query.URL;
