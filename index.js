801e17a203076049efed812414175a590326f3a9
Misha <43975516+oty-v@users.noreply.github.com>
2021-02-13 00:07:19 +0200

    created

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const ytdl = require("ytdl-core");
+const app = express();
+const port = process.env.PORT || 5000;
+
+////////cors()
+
+app.use(cors());
+
+////////port open
+app.listen(port, () => {
+  console.log(`Server Works !!! At port ${port}`);
+});
+
+/////get request handel here get url for one video
+
+app.get("/youtube", (req, res) => {
+  var URL = req.query.URL;
+
+  ytdl
+    .getInfo(URL)
+    .then((info) => {
+      const format = ytdl.filterFormats(info.formats, "audioonly");
+
+      res.json(format[1].url);
+    })
+    .catch((err) => console.log(err));
+});
